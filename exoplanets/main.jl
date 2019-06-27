539a40697f853aebc3596ab9415b6f3d4f7be1d7
Tim Whitaker <camp4climber@gmail.com>
2019-06-27 08:23:43 -0600

    Add stellar svgs

--- exoplanets/main.jl
+++ exoplanets/main.jl
@@ -18,8 +18,40 @@ planets = DataFrame(name = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Sat
 describe(exoplanets)
 describe(planets)
 
-# Mass x Radius
-# Scatter plot
+# Meta Stuff
+############
+
+# How were planets discovered?
+discoveries = plot(dropmissing(exoplanets, [:pl_disc, :pl_discmethod]), x = :pl_disc, color = :pl_discmethod, Geom.line, Stat.histogram, 
+     Scale.y_sqrt,
+     Guide.colorkey(title = "Discovery Method              "),
+     Guide.xlabel("Discovery Year"))
+
+#draw(SVGJS("discoveries.svg", 6inch, 4inch), discoveries)
+
+# Where are they?
+coordinates = unique(dropmissing(exoplanets, [:st_glon, :st_dist]), [:st_glon, :st_dist])
+
+# Farthest and closest
+sorted_distance = sort(dropmissing(exoplanets, [:st_dist]), :st_dist)
+closest = first(sorted_distance)
+farthest = last(sorted_distance)
+
+# Convert polar galactic coordinates to cartesian
+x_pos = coordinates[:st_dist] .* cos.(coordinates[:st_glon])
+y_pos = coordinates[:st_dist] .* sin.(coordinates[:st_glon])
+
+star_map = plot(layer(x = [0, 8121.9961554], y = [0, -7.90263480146], label = ["Earth", "Galactic Center"], Geom.point, Geom.label, style(default_color = colorant"white", point_label_color = colorant"white")),
+     layer(x = x_pos, y = y_pos),
+     Guide.xlabel("Distance (Parsecs)"),
+     Guide.ylabel("Distance (Parsecs)"))
+
+#draw(SVGJS("star-map.svg", 6inch, 4inch), star_map)
+
+# Planet Characteristics
+########################
+
+# Mass Radius Scatter
 scatter = plot(layer(planets, x = :radius, y = :mass, label = :name, Geom.point, Geom.label,
         style(default_color = colorant"white", point_label_color = colorant"white")),
      layer(dropmissing(exoplanets, [:pl_rade, :pl_bmasse]), x = :pl_rade, y = :pl_bmasse),
@@ -27,7 +59,7 @@ scatter = plot(layer(planets, x = :radius, y = :mass, label = :name, Geom.point,
 
 #draw(SVGJS("mass-radius-scatter.svg", 6inch, 4inch), scatter)
 
-# 2d Density plot
+# Mass Radius 2d Density
 density = plot(layer(planets, x = :radius, y = :mass, label = :name, Geom.point, Geom.label,
            style(default_color = colorant"white", point_label_color = colorant"white")),
      layer(dropmissing(exoplanets, [:pl_rade, :pl_bmasse]), x = :pl_rade, y = :pl_bmasse, Geom.density2d),
@@ -37,12 +69,11 @@ density = plot(layer(planets, x = :radius, y = :mass, label = :name, Geom.point,
 
 #draw(SVGJS("mass-radius-density.svg", 6inch, 4inch), density)
 
-# Relative size
-sorted = sort(dropmissing(exoplanets, :pl_rade), :pl_rade)
-smallest = first(sorted)
-largest = last(sorted)
+# Relative Size
+sorted_size = sort(dropmissing(exoplanets, :pl_rade), :pl_rade)
+smallest = first(sorted_size)
+largest = last(sorted_size)
 
-# Values plotted manually
 plot(layer(x = [3.5], y = [0], label = ["Kepler-37 b"], Geom.point, Geom.label, style(point_size = 0.336pt, point_label_color = colorant"white")),
      layer(x = [3], y = [0], label = ["Earth"], Geom.point, Geom.label, style(point_size = 1pt, point_label_color = colorant"white")),
      layer(x = [2.5], y = [0], label = ["Jupiter"], Geom.point, Geom.label, style(point_size = 11.21pt, point_label_color = colorant"white")),
@@ -52,14 +83,13 @@ plot(layer(x = [3.5], y = [0], label = ["Kepler-37 b"], Geom.point, Geom.label, 
 #draw(SVGJS("relative-size.svg", 6inch, 4inch), relativeSize)
 
 # How hot are the planets?
-
 temp = plot(dropmissing(exoplanets, [:pl_eqt, :pl_ratdor, :pl_insol]), x = :pl_ratdor, y = :pl_insol, color = :pl_eqt,
      Scale.y_log10, Scale.x_log10, Scale.color_continuous(colormap = (x->get(ColorSchemes.blackbody, x))),
      Guide.xlabel("Ratio of Distance to Star Size"),
      Guide.ylabel("Solar Irradiance (Earth Flux)"),
      Guide.colorkey(title = "Temp (K) "))
 
-draw(SVGJS("equilibrium-temperature.svg", 6inch, 4inch), temp)
+#draw(SVGJS("equilibrium-temperature.svg", 6inch, 4inch), temp)
 
 # What do their orbits look like?
 semi_major_axis = plot(dropmissing(exoplanets, [:pl_orbsmax]), x = :pl_orbsmax, Geom.histogram(bincount = 50),
@@ -76,17 +106,47 @@ inclination = plot(dropmissing(exoplanets, [:pl_orbincl]), x = :pl_orbincl, Geom
 
 orbits = gridstack([semi_major_axis period; eccentricity inclination])
 
-draw(SVGJS("orbit-grid.svg", 6inch, 4inch), orbits)
+#draw(SVGJS("orbit-grid.svg", 6inch, 4inch), orbits)
 
 # Do they have moons?
 exoplanets[exoplanets[:pl_mnum] .> 0, :pl_mnum] |> length
 
-# How big are the host stars?
+# Stellar Characteristics
+#########################
 
-# How hot are the host stars?
+# How big are they?
+star_size = plot(layer(x = [1], y = [1], label = ["Sun"], Geom.point, Geom.label, style(default_color = colorant"white", point_label_color = colorant"white")),
+     layer(dropmissing(exoplanets, [:st_rad, :st_mass]), x = :st_rad, y = :st_mass),
+     Scale.y_log10, Scale.x_log10,
+     Guide.xlabel("Radius (Solar Radii)"),
+     Guide.ylabel("Mass (Solar Radii)"))
 
-# What are their spectral qualities?
+#draw(SVGJS("star-mass-radius-scatter.svg", 6inch, 4inch), star_size)
 
-# What are they composed of?
+# What are their temps, colors and brightness?
+spectrals = plot(dropmissing(exoplanets, [:st_lum, :st_teff, :st_sp]), y = :st_lum, x = :st_teff, color = :st_teff,
+     Scale.x_log10,
+     Scale.color_continuous(colormap = (x->get(ColorSchemes.blackbody, x))),
+     Guide.xlabel("Effective Temperature (K)"),
+     Guide.ylabel("Luminosity (log(Solar))"),
+     style(key_position = :none),
+     Coord.cartesian(xflip = true))
 
-# How old are they?
+#draw(SVGJS("star-temperature-brightness.svg", 6inch, 4inch), spectrals)
+
+
+# How are age and activity related?
+s_activity = plot(dropmissing(exoplanets, [:st_age, :st_acts]), x = :st_age, y = :st_acts, Geom.histogram(bincount = 30))
+r_activity = plot(dropmissing(exoplanets, [:st_age, :st_actr]), x = :st_age, y = :st_actr)
+x_activity = plot(dropmissing(exoplanets, [:st_age, :st_actlx]), x = :st_age, y = :st_actlx)
+
+activities = vstack([s_activity, r_activity, x_activity])
+
+#draw(SVGJS("star-activity.svg", 6inch, 8inch), activities)
+
+met_fe = plot(dropmissing(exoplanets, [:st_metfe]), x = :st_metfe, Geom.histogram(bincount = 50), Guide.xlabel("Metallicity (Dex)"))
+met_ratio = plot(dropmissing(exoplanets, [:st_metratio]), x = :st_metratio, Geom.histogram, Guide.xlabel("Metallicity Ratio"))
+
+metallicity = hstack([met_fe, met_ratio])
+
+draw(SVGJS("star-metallicity.svg", 6inch, 4inch), metallicity)
