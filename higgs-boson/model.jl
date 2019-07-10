d62338b7bc6a5c3d9d3556ffee222cceebc90772
Tim Whitaker <camp4climber@gmail.com>
2019-07-09 21:22:59 -0600

    Lay out feature engineering ideas

--- higgs-boson/model.jl
+++ higgs-boson/model.jl
@@ -1,9 +1,33 @@
 using CSV, DataFrames, XGBoost
 
-function load_data(filename)
-  data = CSV.read(filename)
-  return data[:, 1:31], map(i -> i == "s" ? 1 : 0, data[:, 33])
-end
+train = CSV.read("higgs-boson/train.csv")
+test = CSV.read("higgs-boson/test.csv")
 
-train_x, train_y = load_data("higgs-boson/train.csv")
-test_x = CSV.read("higgs-boson/test.csv")
+train_x, train_y = train[:, 2:31], map(i->i == "s" ? 1 : 0, train[:Label])
+test_x = test[:, 2:31]
+
+#################################
+# Feature Selection + Engineering
+#################################
+
+# Phi + Eta Angles
+tau_lep_phi
+tau_met_phi
+tau_jet_leading_phi
+lep_met_phi
+lep_jet_leading_phi
+met_jet_leading_phi
+jet_leading_subleading_phi
+
+tau_lep_eta
+tau_jet_leading_eta
+lep_jet_leading_eta
+jet_leading_subleading_eta
+
+# Transverse Momentum Ratios
+tau_lep_pt
+jet_leading_subleading_pt
+met_sumet
+
+# Drop phi due to invariant rotational symmetry
+tau_phi, lep_phi, met_phi, jet_leading_phi, jet_subleading_phi
