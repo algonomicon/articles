9f28374c5021764b7566e77ef2e623100d7c9d35
Tim Whitaker <camp4climber@gmail.com>
2019-07-10 16:56:32 -0600

    Add model test

--- higgs-boson/model.jl
+++ higgs-boson/model.jl
@@ -10,24 +10,50 @@ test_x = test[:, 2:31]
 # Feature Selection + Engineering
 #################################
 
-# Phi + Eta Angles
-tau_lep_phi
-tau_met_phi
-tau_jet_leading_phi
-lep_met_phi
-lep_jet_leading_phi
-met_jet_leading_phi
-jet_leading_subleading_phi
+# Delta phi angles
+# Map to [-pi, pi]
+delta_phi_tau_lep
+delta_phi_tau_jet1
+delta_phi_tau_jet2
+delta_phi_lep_jet1
+delta_phi_lep_jet2
+delta_phi_jet1_jet2
 
-tau_lep_eta
-tau_jet_leading_eta
-lep_jet_leading_eta
-jet_leading_subleading_eta
-
-# Transverse Momentum Ratios
-tau_lep_pt
-jet_leading_subleading_pt
-met_sumet
-
+# Delta Eta angles
+delta_eta_tau_lep
+delta_eta_tau_jet1
+delta_eta_tau_jet2
+delta_eta_lep_jet1
+delta_eta_lep_jet2
+delta_eta_jet1_jet2
+ 
 # Drop phi due to invariant rotational symmetry
-tau_phi, lep_phi, met_phi, jet_leading_phi, jet_subleading_phi
+tau_phi, lep_phi, met_phi, jet1_phi, jet2_phi
+
+# Take absolute values of eta
+tau_eta, lep_eta, jet1_eta, jet2_eta
+
+###############
+# Normalization
+###############
+
+# Center everything around mean of 0 and std dev of 1
+
+###################
+# Raw Feature Model
+###################
+
+# rounds = 2
+# model = xgboost(convert(Matrix, train_x), rounds, label = train_y, eta = 1, max_depth = 2)
+# predictions = predict(model, train_x)
+
+# Test error
+# rmse or ams (higgs eval metric)
+
+# Cross validation
+# nfolds = 5
+# params = Dict("max_depth" => 2, 
+#               "eta" => 1,
+#               "objective" => "binary:logistic")
+
+# folded_model = nfold_cv(train_x, rounds, label = train_y, param = params, metrics = ["auc"])
